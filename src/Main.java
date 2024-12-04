837dea06de8048b5960163e9694bcf4dd2964e53
Netsai <hellonetsai@gmail.com>
2024-12-04 15:47:01 +0200

    Nest 2 d arrays

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,19 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        int [][] numberGrid = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+            {0}
+        };
+        System.out.println(numberGrid[0][1]);
+        for (int i = 0; i< numberGrid.length;i++){
+            for(int j = 0; j < numberGrid[i].length; j++){
+                System.out.print( numberGrid[i][j]);
+            }
+            System.out.println();
+        }
+        
     }
 }
