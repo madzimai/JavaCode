8562b32c263d595a60be1e63ef820e17aff94528
Netsai <hellonetsai@gmail.com>
2024-12-06 11:47:08 +0200

    Call array args  and varags

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Arrays;
+
 public class Main {
+
+    public static void printStormName(String... names) {
+   System.out.println(Arrays.toString(names));
+}
+public static void printStormNames(String[] names) {
+   System.out.println(Arrays.toString(names));
+}
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -10,6 +20,9 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
+            printStormName( "Arlene", "Bret", "Cindy"); 
+            printStormNames(new String[] { "Dennis", "Emily", "Frank" });
         }
     }
 }
